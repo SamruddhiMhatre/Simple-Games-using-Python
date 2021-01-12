7db2f39aa92cb9db90e16ba5b2fb04f8c978a5e2
Samruddhi Mhatre <35568225+SamruddhiMhatre@users.noreply.github.com>
2021-01-12 14:15:43 +0530

    Update guessthenumber.py

--- guessthenumber.py
+++ guessthenumber.py
@@ -1,21 +1,42 @@
-from random import randint
+import random
 
-games = int(input("How many games you would like to play? "))
-
-for game in range(games):
-    ranNum = int(input("Enter a random number: "))
-    value = randint(1,26)
-    
+def guessthenumber():
+    random_num = int(random.randint(1,10))
     attempts = 0
-    
-    while ranNum != value:
-        if ranNum > value:
-            print("Too high!")
-            ranNum = int(input("Enter a random number: "))
-        elif ranNum < value:
-            print("Too low!") 
-            ranNum = int(input("Enter a random number: "))
-        attempts +=1
-    
-    print("You have guessed the number in ", attempts , "attempts!")
-    
+    score = 100
+    user_res = input('Do you want to play Guess the Number? Enter Yes/No: ')
+
+    while user_res == 'yes' or user_res == 'Yes':
+        try:
+            num = int(input('Enter a number between 1 and 10: '))
+            if num < 1 or num > 10:
+                print('Enter the number in given range i.e between 1-10.')
+
+            if num > random_num:
+                attempts += 1
+                score -= 10
+                print('Too high. Your current score is {}'.format(score))
+
+            elif num < random_num:
+                attempts += 1
+                score -= 10
+                print('Too low. Your current score is {}'.format(score))
+
+            elif num == random_num:
+
+                score += 20
+                user_res = input('You have guessed the number in {} attempt(s)! Your current score is {}. Do you wish to continue? Yes/No: '.format(attempts, score))
+                attempts = 0
+                random_num = int(random.randint(1,10))
+
+        except ValueError as err:
+                print("Oh no!, that is not a valid value. Try again :)")
+                print("({})".format(err))
+
+
+    if user_res == 'no' or user_res == 'No':
+        print('See ya!')
+        
+        
+if __name__ == '__main__':
+    guessthenumber()
